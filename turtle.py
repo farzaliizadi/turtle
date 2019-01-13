6fdfdc23725bcb6167bc82f59e2ec6f241e5f4a6
Farzali Izadi <45115436+farzaliizadi@users.noreply.github.com>
2019-01-13 02:04:55 -0500

    Add files via upload

---
+++ turtle.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+for letter in 'Python':     # First Example
+   print('Current Letter :', letter)
+
+for i in range(5):
+    print(i) 
+
+
+import turtle
+turtle.color("red")
+for steps in range(5):
+     turtle.forward(200)
+     turtle.right(360/5)
+     for steps in range(5):
+         turtle.forward(100) 
+         turtle.right(360/5)
+
+
+import turtle         
+turtle.color("green")
+for steps in range(6):
+     turtle.forward(200)
+     turtle.right(360/6)
+     for steps in range(6):
+         turtle.forward(100) 
+         turtle.right(360/6)
+
+
+
+import turtle         
+turtle.color("purple")
+for steps in range(7):
+     turtle.forward(200)
+     turtle.right(360/7)
+     for steps in range(7):
+         turtle.forward(100) 
+         turtle.right(360/7)
+         
+         
+         
+import turtle         
+turtle.color("blue")
+for steps in range(10):
+     turtle.forward(200)
+     turtle.right(360/10)
+     for steps in range(10):
+         turtle.forward(100) 
+         turtle.right(360/10)
+
+
+
+
+
